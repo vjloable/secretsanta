9615482918d172f185c33cf67d3b8d08e3de20ec
Eagl3ye <vincejaphethloable@gmail.com>
2023-07-03 00:07:31 +0800

    refactor: change html to php

--- lobby.php
+++ lobby.php
@@ -10,10 +10,9 @@
       <nav class="navbar navbar-expand-sm navbar-dark bg-danger">
           <div class="container-fluid">
               <a class="navbar-brand" href="#">secretsanta🤫</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
-                  aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+              <input  class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                  aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation"/>
                   <span class="navbar-toggler-icon"></span>
-              </button>
               <div class="collapse navbar-collapse" id="navbarID">
                   <div class="navbar-nav">
                       <a class="nav-link active" aria-current="page" href="#">Home</a>
