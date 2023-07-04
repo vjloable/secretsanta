37eac63038aa658ccd1a805437095e21190ae4d6
Eagl3ye <vincejaphethloable@gmail.com>
2023-07-04 20:09:44 +0800

    refactor: add big center text and change the color choice

--- welcome.php
+++ welcome.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Welcome😎</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    </head>
-    <body>
-        <nav class="navbar navbar-expand-sm navbar-dark bg-danger">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">secretsanta🤫</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID" aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarID">
-                    <div class="navbar-nav">
-                        <a class="nav-link active" aria-current="page" href="/secretsanta/welcome.php">Home</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-        <h1 class="text-center pt-5">Hello! 😁😁😁</h1>
-        <div class="d-flex flex-column align-items-baseline bd-highlight mb-3" style="min-height: 50vh;">
-            <div class="p-2 bd-highlight" style="margin-top: 5%;"></div>
-            <div class="p-2 bd-highlight w-100" style="flex-grow: 1;">
-                <div class="btn-group-lg btn-group-justified text-center h-50" role="group" aria-label="Basic example">
-                    <a class="btn btn-success" style="padding: 5% 5%" href="/secretsanta/signup.php" role="button">Sign up</a>
-                    <a style="padding-left: 15%;"></a>
-                    <a class="btn btn-success" style="padding: 5% 5%" href="/secretsanta/login.php" role="button">Login</a>
-                </div>
-            </div>
-            <div class="p-2 bd-highlight" ></div>
-        </div>
-    </body>
-</html>
+	<head>
+		<title>Welcome😎</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-sm navbar-dark bg-danger" style="height: 7vh">
+			<div class="container-fluid">
+				<a class="navbar-brand" style="font-weight: 900;" href="#">🤫SecretSanta</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID" aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</div>
+		</nav>
+		
+		<div class="d-flex" style="height: 93vh; background-color: rgb(255, 100, 100);">
+			<div class="p-2 flex-fill"></div>
+			<div class="p-2 col-3 flex-fill align-self-center">
+				<div class="mb-5">
+					<h1 class="jumbotron-heading" style="font-size: 90px; font-weight: 900; text-align: center; color: rgb(255, 255, 255);">Secret Santa</h1>
+					<p class="lead text-center" style="color: rgb(255, 255, 255);">
+						A web-based application that facilitates a fair randomization of gift-givers or “santa” in the exchanging of gifts among a group of users.
+					</p>
+				</div>
+				<div class="container-fluid justify-content-center">
+					<div class="btn-group-lg btn-group-justified text-center">
+						<a class="btn btn-light text-danger btn-sm mr-3 py-2" style="width: 160px;"href="/secretsanta/login.php" role="button">LOGIN</a>
+						<a class="btn btn-outline-light btn-sm py-2" style="width: 160px;" href="/secretsanta/signup.php" role="button">SIGN-UP</a>
+					</div>
+				</div>
+			</div>
+			<div class="p-2 flex-fill"></div>
+		</div>
+	</body>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	</html>
