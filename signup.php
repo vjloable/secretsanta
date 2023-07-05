475548188b5e2315044d99fd37165946bb3ac5fb
Elizer Dolorosa <43069251+SuchWowEl@users.noreply.github.com>
2023-07-05 21:20:01 +0800

    refactor: add red-bg and verticalize page title

--- signup.php
+++ signup.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html style="height: 100%;">
+
 <head>
-  <title>Signup Form 📝</title>
+  <title> Signup Form 📝 </title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://kit.fontawesome.com/5d09c7d46f.js" crossorigin="anonymous"></script>
 </head>
+
 <body style="height: 100%;">
   <nav class="navbar navbar-expand-sm navbar-dark bg-danger">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">secretsanta🤫</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
-            aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarID">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="/secretsanta/welcome.php">Home</a>
-            </div>
+      <a class="navbar-brand" href="#">secretsanta🤫</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID" aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarID">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="/secretsanta/welcome.php">Home</a>
         </div>
+      </div>
     </div>
   </nav>
   <i class="fa fa-arrow-left fa-2x p-3" aria-hidden="true"></i>
@@ -30,12 +31,12 @@
           <h2 class="card-title text-center">Signup Form</h2>
         </div>
         <div class="col-md-8">
-          <p class="text-danger text-center">* 
-          <?php
+          <p class="text-danger text-center">*
+            <?php
             if (isset($_GET['error_msg'])) {
               echo urldecode($_GET['error_msg']);
             }
-          ?>
+            ?>
           </p>
           <form method="post">
             <div class="form-group">
@@ -51,4 +52,5 @@
     </div>
   </div>
 </body>
+
 </html>
