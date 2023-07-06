885f568c51354066272289a000663e1c3db57fea
Elizer Dolorosa <43069251+SuchWowEl@users.noreply.github.com>
2023-07-07 02:48:21 +0800

    refactor: fix nav + table, add state

--- join_2.php
+++ join_2.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/5d09c7d46f.js" crossorigin="anonymous"></script>
     <style>
         .table-wrapper-scroll-y {
             max-height: 200px;
@@ -50,14 +51,22 @@
 <body style="background-color: rgb(255, 100, 100);">
     <nav class="navbar navbar-expand-sm navbar-dark bg-danger" style="height: 7vh">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">secretsanta🤫</a>
-            <input class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID" aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation" />
-            <span class="navbar-toggler-icon"></span>
-            <div class="collapse navbar-collapse" id="navbarID">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="/secretsanta/welcome.php">Home</a>
+            <a class="navbar-brand" style="font-weight: 900;" href="#">🤫SecretSanta</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID" aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="dropdown">
+                <button class="btn dropdown-toggle" style="color: white;" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Account Settings</a>
+                    <div class="dropdown-divider" style="background-color: #555; height: 1px; margin: 5px 0;"></div>
+                    <a class="dropdown-item" href="#" style="color: red;">Logout</a>
                 </div>
             </div>
+
+            <!-- <i class=" fa-solid fa-bars" style="color: white;" aria-hidden="true"></i> -->
         </div>
     </nav>
 
